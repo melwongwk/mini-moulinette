6d43c7854909c9ccbddb94e2bd33a78112ae640d
Hanchai Nonprasart <hnonpras@z2t9c2.42bangkok.com>
2023-06-13 12:35:44 +0700

    feat: implement test for C05/ex08

--- mini-moul/tests/C05/ex08/ft_ten_queens_puzzle.c
+++ mini-moul/tests/C05/ex08/ft_ten_queens_puzzle.c
@@ -1,25 +1,65 @@
+#include "../../../utils/constants.h"
+#include "ft_ten_queens_puzzle.h"
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <fcntl.h>
-#include "../../../../ex08/ft_ten_queens_puzzle.c"
-#include "../../../utils/constants.h"
+#include <sys/stat.h>
+#include <unistd.h>
 
-int test1(void);
+int	test1(void);
 
-int main(void)
+int	main(void)
 {
 	if (test1() != 0)
 		return (-1);
 	return (0);
 }
 
-int test1(void)
-{	
-	if (1 > 0)
+int	test1(void)
+{
+	char	buffer[1024];
+	int		saved_stdout;
+	int		output_fd;
+	int		result;
+	int		expected;
+	FILE	*fp;
+
+	// Flush the standard output buffer
+	fflush(stdout);
+	// Clear the buffer used to capture the output of the function being tested
+	memset(buffer, 0, sizeof(buffer));
+	// Redirect the output to a file
+	saved_stdout = dup(STDOUT_FILENO);
+	output_fd = open("output.txt", O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	dup2(output_fd, STDOUT_FILENO);
+	close(output_fd);
+	result = ft_ten_queens_puzzle();
+	expected = 724;
+	// Restore the original output
+	fflush(stdout);
+	dup2(saved_stdout, STDOUT_FILENO);
+	close(saved_stdout);
+	// Open the output file and check its contents
+	fp = fopen("output.txt", "r");
+	fgets(buffer, sizeof(buffer), fp);
+	fclose(fp);
+	if (result != expected)
 	{
-		printf("    " RED "[1] Sorry, test not implemented yet.\n" DEFAULT,);
+		printf("    " RED "[1] ft_ten_queens_puzzle() Expected %d, got %d\n",
+				expected,
+				result);
 		return (-1);
 	}
-	return (0);
+	else
+	{
+		printf("  " GREEN CHECKMARK GREY
+				" [1] ft_ten_queens_puzzle() Expected %d, got %d\n" DEFAULT,
+				expected,
+				result);
+		return (0);
+	}
+	// Delete the output file
+	remove("output.txt");
 }
---
+++ mini-moul/tests/C05/ex08/ft_ten_queens_puzzle.h
@@ -0,0 +1,6 @@
+#ifndef FT_TEN_QUEENS_PUZZLE
+# define FT_TEN_QUEENS_PUZZLE
+
+int	ft_ten_queens_puzzle(void);
+
+#endif
