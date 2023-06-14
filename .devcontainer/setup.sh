e52613be7b7c19ca71130567c25c67199c954a55
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2023-06-14 16:26:41 +0000

    build: fixup
    Set  alias in setup.sh.
    
    Update settings.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,7 @@
 
 python3 -m pip install --upgrade pip setuptools
 pip3 install norminette
-ln -s /workspaces/mini-moulinette ~/mini-moulinette
 git clone https://github.com/cacharle/c_formatter_42 ~/c_formatter_42
 pip3 install -e ~/c_formatter_42
+ln -s /workspaces/mini-moulinette ~/mini-moulinette
+echo "alias mini='~/mini-moulinette/mini-moul.sh'" >> ~/.bashrc && source ~/.bashrc
